4729442324f96cac3f4dbc94ae4a419d16629433
VinayChintakayala25 <vinaychintakayala75@gmail.com>
2026-06-12 23:42:37 +0530

    added a comment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import './styles/global.css';
 import 'animate.css';
+//import
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
